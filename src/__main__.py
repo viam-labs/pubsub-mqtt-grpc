ac6552266155811fd6ddd10c69a1247a6f1e9060
Nick Hehr <headhipster@hipsterbrown.com>
2025-01-21 12:54:28 -0500

    fix: update viam-sdk, build module with pyinstaller (#1)

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,9 @@
 import asyncio
-import sys
 
 from viam.module.module import Module
 from pubsub_python import Pubsub
-from .pubsub import mqttGrpc
+from pubsub import mqttGrpc
+
 
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
@@ -12,6 +12,7 @@ async def main():
     module = Module.from_args()
     module.add_model_from_registry(Pubsub.SUBTYPE, mqttGrpc.MODEL)
     await module.start()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
